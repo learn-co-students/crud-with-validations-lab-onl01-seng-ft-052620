d7de4297587e9ae7645fa1168f0be99d4196758c
Luca <lucakanyo@protonmail.com>
2020-08-12 12:48:56 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,17 @@
 class Song < ApplicationRecord
-    
     validates :title, presence: true
     validates :release_year, presence: true, if: :released
     validates :title, uniqueness: { scope: %i[release_year artist_name], message: "should happen once per year"}
-    validates :release_year, future: false
+
+    validate :no_future_dates
+    private 
+
+    def no_future_dates 
+        if self.release_year && self.release_year > Date.today.year 
+            self.errors.add(:release_year, "Cant be in the future")
+        end 
+    end 
+    
     
 end 
     
